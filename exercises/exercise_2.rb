0cf9edf15f69493592c561aacacb4d59556e5810
vagrant <mikefyke@hotmail.com>
2019-05-16 02:57:19 +0000

    Updated the first store completed Ex2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load the first store from the DB id = 1
+@store1 = Store.find_by(id:1)
+# 3. Update the fist store with another name
+@store1.update(name: "Peterborough")
+# 2. Load the second store from the DB and assign it to @store2
+@store2 = Store.find_by(id:1)
