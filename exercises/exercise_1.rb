a7381be8cc33cf5f0fffa196aae02e31fcef695a
vagrant <mikefyke@hotmail.com>
2019-05-16 02:47:54 +0000

    Created 3 stores completed ex1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+#Burnaby (annual_revenue: 300000, items: mens_apparel, womens_apparel)
+#Richmond (annual_revenue: 1260000, items: womens_apparel)
+#Gastown (annual_revenue: 190000, items: mens_apparel)
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true )
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true )
+
+# Outputs (puts) the number of stores using ActiveRecord's
+# count method, to ensure ther are only three stores in the DB.
+puts Store.count
