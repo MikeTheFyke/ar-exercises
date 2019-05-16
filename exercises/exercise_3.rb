052e84452fc505988bae7ec78fdfbc2f2cb7eb88
vagrant <mikefyke@hotmail.com>
2019-05-16 03:01:47 +0000

    loaded and destroyed store3 complete

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load the third store into store3 like the two before.
+@store3 = Store.find_by(id:3)
+# 2. Using ActiveRecords destroy method deletes store from the database.
+@store3.destroy
+# 3. verify the store has been deleted by again outputting
+# (puts) the count like excercise 1.
+puts Store.count
